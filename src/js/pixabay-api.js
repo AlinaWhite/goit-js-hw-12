fb93b9b6c7bf0aeacb54ff00935bdfda31c1ca44
Alina Lanovetska <te091094lam@gmail,com>
2024-11-13 23:00:03 +0000

    console err amf scrool fixinh needed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,20 @@
 import axiox from "axios";
 
-const URL = 'https://pixabay.com/api/';
+axiox.defaults.baseURL = 'https://pixabay.com/api/';
 
 
-export const fetchImages = searchValueImput => {
+export const fetchImages = (searchValueImput, nextPageNumber) => {
     const urlParams = {
         params:{
         q: searchValueImput,
         image_type: 'photo',
         orientation: 'horizontal',
+        per_page: 16,
+        page: nextPageNumber,
         safesearch: true,
         key: '46938898-491ce0dea342e81df1da49dcf',}
     };
 
 
-return axiox.get(`${URL}`,urlParams);
+return axiox.get('',urlParams);
 };
--- src/main.js
+++ src/main.js
@@ -9,19 +9,23 @@ import axiox from "axios";
 const serchForm = document.querySelector('.search-form');
 const galerySerch = document.querySelector('.gallery');
 const loaderQ = document.querySelector('.loader')
+const loadMore = document.querySelector('.load-button')
+
 
 let gallery = new SimpleLightbox('.gallery a');
+let currentPage =1;
+let searchValue ='';
 
 const submitForm = async event => {
     try {
         event.preventDefault();
 
-        const searchValue = serchForm.elements.searchQuery.value.trim();
+        searchValue = serchForm.elements.searchQuery.value.trim();
         
         loaderQ.classList.remove('is-hidden');
 
-        const response =  await fetchImages(searchValue);
-
+        const response =  await fetchImages(searchValue, currentPage);
+        
         if (response.data.hits.length === 0){
               iziToast.error ({
                 message: "Sorry, there are no images matching your search query. Please try again!",
@@ -35,8 +39,8 @@ const submitForm = async event => {
 
          const imgCard = response.data.hits.map(imgDetails => galleryCard(imgDetails)).join('');
          galerySerch.innerHTML = imgCard;
+         loadMore.classList.remove('is-hidden');
          gallery.refresh(); 
-
     } catch (err) {
         console.log(err);
     } finally {
@@ -44,4 +48,45 @@ const submitForm = async event => {
     }
 };
 
+const onLoadMoreBtn = async event => {
+    currentPage++;
+    try {
+        loaderQ.classList.remove('is-hidden');
+        const response =  await fetchImages(searchValue, currentPage);
+
+        const imgCard = response.data.hits
+        .map(imgDetails => galleryCard(imgDetails))
+        .join('');
+        currentPage =1;
+
+        galerySerch.insertAdjacentHTML('beforeend' ,imgCard);
+        gallery.refresh(); 
+
+
+        const galleryEl = document.querySelector('gallery-item__image');
+        const imgHeight = galleryEl.getBoundingClientRect();
+        window.scrollBy({
+            top: 2 * imgHeight,
+            behavior: 'smooth',
+        });
+
+        if (response.data.hits >= totalHits){
+            loadMore.classList.add('is-hidden');
+            iziToast.error ({
+              message: "We're sorry, but you've reached the end of search results.",
+              position: 'topRight',
+           });
+            return
+          }
+        
+    } catch(err){
+        console.log(err);
+    } finally{
+        loaderQ.classList.add('is-hidden');
+    }
+
+};
+
+
     serchForm.addEventListener('submit', submitForm);
+    loadMore.addEventListener('click', onLoadMoreBtn);
